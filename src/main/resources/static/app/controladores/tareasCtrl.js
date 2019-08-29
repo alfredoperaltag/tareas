340913648ebd47fd0b5273f0ef03ee72f22331cc
alfredoperaltag <alfredo08peralta@gmail.com>
2019-08-29 11:31:41 -0500

    agregar

--- src/main/resources/static/app/controladores/tareasCtrl.js
+++ src/main/resources/static/app/controladores/tareasCtrl.js
@@ -1,12 +1,125 @@
-app.controller('tareasCtrl', function ($scope, tareasService) {
-	$scope.suma = 3 + 3;
+app.controller('tareasCtrl', function ($scope, tareasService, $location) {
+    $scope.suma = 3 + 3;
+    //$scope.tarea = {};
+    $scope.listarTareas = {};
+    $scope.tareaEditar = {};
 
-	$scope.listarTareas = {};
+    $scope.verSeccion = function () {
+//        $scope.accion = accion;
+    	$scope.tarea= {};
+    };
 
-	tareasService.get().then((data) => {
-		console.log(data)
-		$scope.listarTareas = data;
-	}, (reject) => {
-		console.log(reject);
-	});
+    
+
+    $scope.obtenerTarea = function(){
+    	tareasService.get().then((data) => {
+            console.log("Ctrl: ", data);
+            $scope.listarTareas = data;
+        }, (reject) => {
+            console.log("Ctrl: ", reject);
+        });
+    }
+    
+
+//    $scope.tarea.titulo=$scope.titulo;
+//    $scope.tarea.fecha=$scope.fecha;
+//    $scope.tarea.descripcion=$scope.descripcion;
+    
+    
+    $scope.submitForm= function(esValido){
+    	if(esValido){
+    		if($scope.tarea.id){
+    			//aqui se actualiza la tarea
+    			$scope.editarTarea();
+    			
+    		}else{
+    			//aqui se registra
+    			$scope.enviarTarea();
+    		}
+    	} else {
+            Swal.fire(
+                    '¡error!',
+                    '¡Faltan Datos!',
+                    'error'
+                )
+            }
+    }
+    
+    $scope.enviarTarea = function () {
+        
+        
+        console.log("tarea",$scope.tarea);
+        
+            tareasService.post($scope.tarea).then((respuesta) => {
+            	console.log("respuesta",respuesta);
+                if (respuesta===true) {
+                	console.log(respuesta);
+                	$scope.tarea = {};
+                    Swal.fire(
+                        '¡OK!',
+                        '¡Se registro correctamente!',
+                        'success'
+                    )
+                    $scope.tarea=null;
+                    $scope.obtenerTarea();
+                } else{
+                	console.log("ERROR");
+                }
+                //console.log("post: ", scope.archivo);
+                //$scope.listarTareas = data;
+            }, (reject) => {
+                console.log("Ctrl: ", reject);
+            });
+        
+    }
+    
+    $scope.iniciarEdicion = function (tarea) {
+        $scope.tarea = tarea;
+        $scope.tarea.fecha = new Date(tarea.fecha)
+//        $scope.verSeccion('Editar');
+    }
+    
+
+    $scope.editarTarea = function (esValido) {
+        if (esValido) {
+        
+        console.log("tareaEditar",$scope.tareaEditar);
+        
+            tareasService.put($scope.tareaEditar).then((respuesta) => {
+            	console.log("respuesta",respuesta);
+                if (respuesta===true) {
+                	console.log(respuesta);
+                	//$scope.tarea = {};
+                    Swal.fire(
+                        '¡OK!',
+                        '¡Se edito correctamente!',
+                        'success'
+                    )
+                    $scope.verSeccion('Lista');
+                } else{
+                	console.log("ERROR");
+                }
+                //console.log("post: ", scope.archivo);
+                //$scope.listarTareas = data;
+            }, (reject) => {
+                console.log("Ctrl: ", reject);
+            });
+        } else {
+            Swal.fire(
+                '¡error!',
+                '¡Faltan Datos!',
+                'error'
+            )
+        }
+    }
+    
+    const initController = function(){
+    	$scope.obtenerTarea();
+    }
+    
+    angular.element(document).ready(function (){
+    	initController();
+    })
+    
+    
 })
--- src/main/resources/static/app/servicios/tareasService.js
+++ src/main/resources/static/app/servicios/tareasService.js
@@ -14,5 +14,39 @@ app.service ('tareasService', function($q, factory) {
                 })
         })
     }
+    SELF.post = (tarea) => {
+        return $q((success, error) => {
+            factory.post(PATH, tarea).then(
+                (resolve) => {
+                    success(resolve)
+                },
+                (reject) => {
+                    error(reject)
+                })
+        })
+    }
+    SELF.put = (tarea) => {
+        return $q((success, error) => {
+            factory.put(PATH, tarea).then(
+                (resolve) => {
+                    success(resolve)
+                },
+                (reject) => {
+                    error(reject)
+                })
+        })
+    }
+    SELF.delete = (tarea) => {
+        return $q((success, error) => {
+            factory.delete(PATH, tarea).then(
+                (resolve) => {
+                    success(resolve)
+                },
+                (reject) => {
+                    error(reject)
+                })
+        })
+    }
+    
     // realizar conttrolador y terminar el REST
 })
